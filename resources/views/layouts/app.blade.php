427d4f57a30f99e9d66bb9d3519a6bee17b9290b
Farditbee <farhandito15@gmail.com>
2025-09-11 15:34:27 +0700

    icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        
+        <title>mY Mutasi</title>
+        
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" sizes="128x128" href="{{ asset('img/logo.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
